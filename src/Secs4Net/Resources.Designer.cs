ccf95b3495d3bc7bf104051dcdb3b9222b0b8730
Bo Hu <hubo611@foxmail.com>
2023-12-12 17:32:44 +0800

    update

--- src/Secs4Net/Resources.Designer.cs
+++ src/Secs4Net/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace Secs4Net {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Secs4Net.Bert.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Secs.Bert.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
